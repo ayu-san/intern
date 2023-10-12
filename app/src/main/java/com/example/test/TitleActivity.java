f6022b2c78d58c259d6d2ae83acd47df69b661f4
aikawa <aikawayuusei1118@gmail.com>
2023-10-12 11:58:11 +0900

    タップ時エフェクト追加（タイトルのみ）

--- app/src/main/java/com/example/test/TitleActivity.java
+++ app/src/main/java/com/example/test/TitleActivity.java
@@ -1,11 +1,19 @@
 package com.example.test;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 
@@ -21,6 +29,8 @@ public class TitleActivity extends AppCompatActivity {
     private ImageView titlLogo;
 
     private SoundPlayer soundPlayer;
+    private FrameLayout frameLayout;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +46,19 @@ public class TitleActivity extends AppCompatActivity {
         titlLogo.startAnimation(floatAnimation);
 
         soundPlayer = new SoundPlayer(this);
+
+        frameLayout = findViewById(R.id.tap_effect);
+
+        frameLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    ShowTouchEffect(motionEvent.getX(), motionEvent.getY());
+                }
+
+                return false;
+            }
+        });
 
         //始めるボタンを押したとき
         startButton.setOnClickListener((View v)->{
@@ -180,6 +203,60 @@ public class TitleActivity extends AppCompatActivity {
                 alertDialog.dismiss();
             }
         }
+    }
+
+    private void ShowTouchEffect(float x, float y){
+        // エフェクト用の ImageView を作成
+        ImageView effectImageView = new ImageView(this);
+        effectImageView.setImageResource(R.drawable.tap_effect_drawable); // エフェクト画像を設定
+        effectImageView.setX(x-40); // X 座標を設定
+        effectImageView.setY(y-80); // Y 座標を設定
+
+// 初期サイズを設定
+        int initialSize = 180;
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(initialSize, initialSize);
+        effectImageView.setLayoutParams(layoutParams);
+
+        frameLayout.addView(effectImageView);
+
+        // アニメーション: エフェクトのサイズを縮小
+        int finalSize = 100; // 最終的なサイズを設定
+        ValueAnimator widthAnimator = ValueAnimator.ofInt(initialSize, finalSize);
+        ValueAnimator heightAnimator = ValueAnimator.ofInt(initialSize, finalSize);
+
+        widthAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                int value = (int) valueAnimator.getAnimatedValue();
+                ViewGroup.LayoutParams layoutParams = effectImageView.getLayoutParams();
+                layoutParams.width = value;
+                effectImageView.setLayoutParams(layoutParams);
+            }
+        });
+
+        heightAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                int value = (int) valueAnimator.getAnimatedValue();
+                ViewGroup.LayoutParams layoutParams = effectImageView.getLayoutParams();
+                layoutParams.height = value;
+                effectImageView.setLayoutParams(layoutParams);
+            }
+        });
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(widthAnimator, heightAnimator);
+        animatorSet.setDuration(300); // アニメーションの時間（ミリ秒）
+        animatorSet.start();
+
+        // エフェクトのアニメーション（例：フェードアウト）
+        effectImageView.animate().alpha(0).setDuration(300).withEndAction(new Runnable() {
+            @Override
+            public void run() {
+                // エフェクトをコンテナから削除
+                frameLayout.removeView(effectImageView);
+            }
+        });
     }
 
 }
